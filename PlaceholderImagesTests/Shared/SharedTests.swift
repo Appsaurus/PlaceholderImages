34922937b99c952f1edc015183c178a45845fcce
Brian Strobach <brian@appsaurus.io>
2018-05-28 22:19:04 -0500

    - Added a couple more tests for each provider

--- PlaceholderImagesTests/Shared/SharedTests.swift
+++ PlaceholderImagesTests/Shared/SharedTests.swift
@@ -8,41 +8,55 @@ class SharedTests: BaseTestCase {
 	//MARK: Linux Testing
 	static var allTests = [
 		("testLinuxTestSuiteIncludesAllTests", testLinuxTestSuiteIncludesAllTests),
-		("testUnsplahURLProvider", testUnsplahURLProvider),
-		("testLoremFlickrURLProvider", testLoremFlickrURLProvider)
+		("testUnsplahURLProvider", testUnsplashURLProvider),
+		("testLoremFlickrURLProvider", testLoremFlickrURLProvider),
+		("testLoremPixelURLProvider", testLoremPixelURLProvider),
+		("testPlaceImgURLProvider", testPlaceImgURLProvider),
+		("testDummyImageURLProvider", testDummyImageURLProvider)
 	]
 
 	func testLinuxTestSuiteIncludesAllTests(){
 		assertLinuxTestCoverage(tests: type(of: self).allTests)
 	}
 
-	public func testUnsplahURLProvider() throws{
-		let url: URL = .imageURL(provider: .unsplash(keywords: ["mountain", "river"]))
-		XCTAssertNotNil(try? Data(contentsOf: url))
+	var keywords: [String] = ["mountain", "river"]
+	public func testUnsplashURLProvider() throws{
+		try test(.imageURL(provider: .unsplash()))
+		try test(.imageURL(provider: .unsplash(keywords: keywords)))
 	}
 
 	public func testLoremFlickrURLProvider() throws{
-
-//	let url: URL = <#url#>
-//	XCTAssertEqual(<#hardcoded#>, url.absoluteString)
-//	let imageData = try? Data(contentsOf: url)
-//	XCTAssertNotNil(imageData)
+		try test(.imageURL(provider: .loremFlickr()))
+		try test(.imageURL(provider: .loremFlickr(keywords: keywords)))
 	}
 
 
-//	public func test<#provider#>URLProvider() throws{
-//
-//	let url: URL = <#url#>
-//	XCTAssertEqual(<#hardcoded#>, url.absoluteString)
-//	let imageData = try? Data(contentsOf: url)
-//	XCTAssertNotNil(imageData)
-//	}
-//
-//	public func test<#provider#>URLProvider() throws{
-//
-//	let url: URL = <#url#>
-//	XCTAssertEqual(<#hardcoded#>, url.absoluteString)
-//	let imageData = try? Data(contentsOf: url)
-//	XCTAssertNotNil(imageData)
-//	}
+	public func testLoremPixelURLProvider() throws{
+		try test(.imageURL(provider: .loremPixel()))
+		try test(.imageURL(provider: .loremPixel(category: .animals)))
+	}
+
+	public func testPlaceImgURLProvider() throws{
+		try test(.imageURL(provider: .placeImg()))
+		try test(.imageURL(provider: .placeImg(category: .animals)))
+		try test(.imageURL(provider: .placeImg(category: .animals, effect: .sepia)))
+	}
+
+	public func testDummyImageURLProvider() throws{
+		try test(.imageURL(provider: .dummy()))
+	}
+
+	func test(_ url: URL) throws {
+		let imageData = try? Data(contentsOf: url)
+		print(url.absoluteString)
+		XCTAssertNotNil(imageData)
+	}
+	//
+	//	public func test<#provider#>URLProvider() throws{
+	//
+	//	let url: URL = <#url#>
+	//	XCTAssertEqual(<#hardcoded#>, url.absoluteString)
+	//	let imageData = try? Data(contentsOf: url)
+	//	XCTAssertNotNil(imageData)
+	//	}
 }
